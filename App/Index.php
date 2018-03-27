1357963bd6bc4d79f9bee4302507bbcae316353d
Sebastian Lind <lindse@student.chalmers.se>
2018-03-27 17:05:29 +0200

    show

--- App/Index.php
+++ App/Index.php
@@ -12,9 +12,10 @@ and open the template in the editor.
     </head>
     <body>
       <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Recycle_Rush.svg/1200px-Recycle_Rush.svg.png" alt="recyle" style="width:700px;height:500px;">
-      <?php
-      $hello = "hello world";
-      echo $hello;
+      <p> Hello World </p> 
+     <?php
+      //$hello = "hello world";
+      //echo $hello;
       ?>
     </body>
 </html>
