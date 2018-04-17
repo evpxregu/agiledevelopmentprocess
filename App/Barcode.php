5e59fb0e7e1ed5a85d9ad2a8cba750e941ac4139
Oscar Jönsson <jooscar@student.chalmers.se>
2018-04-17 14:34:49 +0200

    Added Coke Zero

--- App/Barcode.php
+++ App/Barcode.php
@@ -16,6 +16,10 @@ else if ($code_type === 'ean_13' && $code_number === '0856991004103') {
     echo "N!ck's kexbar! sort as plastics!"; 
 }
 
+else if ($code_type === 'ean_13' && $code_number === '5449000131836') {
+    echo "Coca Cola ZERO! sort as plastics!"; 
+}
+
 else if($code_type === 'ean_13' && $code_number === '5119417807081')
 {
     echo 'Yeah';
