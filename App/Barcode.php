70cb9eb53657458020a217c38ed8c3b068cec509
Rick <nofound@example.com>
2018-05-02 11:35:20 +0200

    added barcode scanning logging

--- App/Barcode.php
+++ App/Barcode.php
@@ -1,12 +1,27 @@
 <?php
+require_once('inc/LogDB.php.inc');
 //lets start with some validation of the submitted parameters
 $codetype = filter_input(INPUT_POST,'code_type',FILTER_SANITIZE_SPECIAL_CHARS);
 $codenumber = filter_input(INPUT_POST,'code_number',FILTER_SANITIZE_SPECIAL_CHARS);
 
+
+if(session_id()  == '')
+{
+    session_start();
+}
 //todo: move all of this to some nice MVC controller like structure which is actually database backed. 
 //For now we are just going to give some data back for my package of "stroopwafelen" which is the only thing i have for testing
 
-function getResult($code_type,$code_number){
+function getResult($code_type,$code_number)
+{
+    
+    
+    //todo: move the code below to the block that is actually ran when a barcode is succesfully found 
+    $logging = new LogDB();
+    //todo keep track of client on user side
+    $logging->Add(session_id(),$code_number );
+    
+    
     
     if($code_type === 'ean_13' && $code_number === '5410091692278')
     {
@@ -42,9 +57,11 @@ function getResult($code_type,$code_number){
     {
         return 'could not find any information';
     }
-
+    
+    
 }
 $result = getResult($codetype,$codenumber);
+
 if($result ==='could not find any information')
 {
     http_response_code(404);
