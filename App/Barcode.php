0f83e95bf6bea25e8ff927e1d50cab05f8fc358c
Sebastian Lind <lindse@student.chalmers.se>
2018-05-03 14:22:44 +0200

    Started on read csv file, commiting so i can pull this dont work

--- App/Barcode.php
+++ App/Barcode.php
@@ -3,11 +3,11 @@
 $codetype = filter_input(INPUT_POST,'code_type',FILTER_SANITIZE_SPECIAL_CHARS);
 $codenumber = filter_input(INPUT_POST,'code_number',FILTER_SANITIZE_SPECIAL_CHARS);
 
-//todo: move all of this to some nice MVC controller like structure which is actually database backed. 
+//todo: move all of this to some nice MVC controller like structure which is actually database backed.
 //For now we are just going to give some data back for my package of "stroopwafelen" which is the only thing i have for testing
 
 function getResult($code_type,$code_number){
-    
+
     if($code_type === 'ean_13' && $code_number === '5410091692278')
     {
         //note RH: return HTML so we can later switch to an android application easier using some webviewer or an AJAX or support for a version for jsblock users with degraded experience.
@@ -15,19 +15,19 @@ function getResult($code_type,$code_number){
     }
 
     else if ($code_type === 'ean_13' && $code_number === '0856991004103') {
-        return "N!ck's kexbar! sort as plastics!"; 
+        return "N!ck's kexbar! sort as plastics!";
     }
 
     else if ($code_type === 'ean_13' && $code_number === '5449000131836') {
-        return "Coca Cola ZERO! sort as plastics!"; 
+        return "Coca Cola ZERO! sort as plastics!";
     }
-    
+
     else if ($code_type === 'ean_13' && $code_number === '7311250004346') {
-        echo "General XR White sort as plastic!"; 
+        echo "General XR White sort as plastic!";
     }
 
     else if ($code_type === 'ean_13' && $code_number === '7311250004551') {
-        echo "General XR Original sort as plastic!"; 
+        echo "General XR Original sort as plastic!";
     }
 
     else if($code_type === 'ean_13' && $code_number === '5119417807081')
@@ -38,16 +38,25 @@ function getResult($code_type,$code_number){
     {
         return 'super huge barcode';
     }
-    else
+    else if($code_type === 'ean_13' && $code_number === '7340116870030')
     {
-        return 'could not find any information';
+      return 'pant';
     }
+    else if($code_type === 'ean_13' && $code_number === '5119417807111')
+    {
+      return 'boy we did it';
+    }
+  //  else
+    //{
+      //    return '1';
+    //}
 
 }
+
 $result = getResult($codetype,$codenumber);
 if($result ==='could not find any information')
 {
     http_response_code(404);
-    
+
 }
 echo $result;
--- App/Test/BarcodeTests.php
+++ App/Test/BarcodeTests.php
@@ -2,8 +2,14 @@
 
 
 require('../Barcode.php');
+// Tests that check if we recive correct barcode info from database.
 
 assert(getResult('ean_13','5119417807081') == 'wood,burn it','value should be equal to eachother');
 
+assert(getResult('ean_13','5119417807111') == 'boy we did it', 'unexpcted result' );
 
-assert(getResult('ean_13','5119417807111') == 'could not find any information', 'unexpcted result' );
+assert(getResult('ean_13', '7340116870030') == 'pant', 'do not recive corret info');
+
+assert(getResult('ean_13', '15') == '20', 'Error test 1');
+
+echo "if you only se this, all test passed. \n Good job! :D";
