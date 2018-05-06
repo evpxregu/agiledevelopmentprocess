b8d4fad66c0bca9023791f03fb92015a15af7510
Murtada <murtadaosama@gmail.com>
2018-05-06 10:22:18 +0200

    removed unnecessary scansearch tab

--- App/Index.php
+++ App/Index.php
@@ -50,23 +50,13 @@ session_start();
                         <div class="tab">
                           <button class="tablinks" id="defaultOpen" onclick="openTab(event, 'Camera')">Scan<i class="material-icons">camera_enhance</i></button>
                           <button class="tablinks" onclick="openTab(event, 'Manual')">Search<i class="material-icons">search</i></button>
-                          
-                          <button class="tablinks" onclick="openTab(event, 'Scan Search')">Scan Search<i class="material-icons">search</i></button>
-                          
-                          
                           <button class="tablinks" onclick="openTab(event, 'Add')">Add<i class="material-icons">note_add</i></button>
                           <button class="tablinks" onclick="openTab(event, 'History')">History<i class="material-icons">list</i></button>
                         </div>
 
                         <!-- Tab content -->
                         
-                        <div id ="Scan Search" class="tabcontent">
-                            <div id="interactive" class="viewport">
-
-                            </div>
-                            <h1><div id="scanresult"></div></h1>
-
-                        </div>
+                        
 
                         <div id="Camera" class="tabcontent">
                             <div id="interactive" class="viewport">
